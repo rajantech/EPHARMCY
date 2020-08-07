2c806ee3c4b92514902451af9a3b613a7dd888ff
rajantech <66750706+rajantech@users.noreply.github.com>
2020-08-07 01:55:21 -0400

    splash,login,signup

---
+++ app/src/main/java/com/example/epharma/customerLogIn.java
@@ -0,0 +1,22 @@
+package com.example.epharma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class customerLogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_log_in);
+    }
+
+    public void gotosignup(View view) {
+        Intent intent=new Intent(this,signUp.class);
+        startActivity(intent);
+    }
+
+}
